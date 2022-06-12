65f30388b5262381161b128e82ffa3d545f910a7
Bartosz Truszkowski <truszkoo@gmail.com>
2022-06-12 12:48:07 +0200

    Initiall commit

---
+++ PersonWebApi/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonWebApi.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("XDdd");
+            return View();
+
+        }
+    }
+}
